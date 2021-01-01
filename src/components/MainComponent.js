ca495af5120a746ddcafb972fc9a0bf612a9620e
Brunilda Lulaj <brunalualaj@gmail.com>
2021-01-01 11:40:40 -0500

    commit changes

---
+++ src/components/MainComponent.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import Home from './HomeComponent';
+import Contact from "./ContactComponent";
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+class Main extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+
+        const HomePage = () => {
+            return (
+                <Home />
+            );
+        };
+
+        return (
+            <div>
+                <Header />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route path='/contactus' component={Contact} />
+                    <Redirect to='/home' />
+                </Switch>
+                <Footer />
+            </div>
+        );
+    }
+}
+
+
+
+export default Main;
